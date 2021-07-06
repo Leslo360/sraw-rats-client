89b592fe2b2ea2467b11caa6ffc284b1d98c6613
SITS Holdings <leslo_360@SITS.holdings>
2021-07-07 00:24:44 +0200

    fixed backend endpoint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const  client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: "https://blooming-mesa-27666.herokuapp.com/"
+  uri: "https://blooming-mesa-27666.herokuapp.com/graphql"
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
